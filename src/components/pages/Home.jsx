f9dea62290be8ef94948e01e51049c466bf12b5f
Mateus-olv-dev <mateusolv.dev@gmail.com>
2026-09-18 07:36:50 -0300

    Implementa responsividade

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home () {
       <h1 className='nome'>Mateus Oliveira,</h1>
       <h2>estudante de Desenvolvimento de Software</h2>
       <p className="resume">
-        Desenvolvedor em formação e participante do projeto <span>Jovem Tech</span>. Tenho interesse em desenvolvimento de software, explorando tanto a criação de interfaces quanto a lógica por trás das aplicações. Atualmente, estudo <span>Java</span> e pratico desenvolvimento de interfaces com <span>React</span>, sempre buscando transformar ideias em soluções funcionais.
+        Desenvolvedor em formação e participante do programa <span>Jovem Tech</span>. Tenho interesse em desenvolvimento de software, explorando tanto a criação de interfaces quanto a lógica por trás das aplicações. Atualmente, estudo <span>Java</span> e pratico desenvolvimento de interfaces com <span>React</span>, sempre buscando transformar ideias em soluções funcionais.
  
       </p>
       <p className='embla-title'>Algumas das tecnologias que utilizo</p>
