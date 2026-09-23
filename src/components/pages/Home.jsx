200762c57183ce573ad2fffb44cc2c6c36f326a6
Mateus-olv-dev <mateusolv.dev@gmail.com>
2026-09-22 21:59:56 -0300

    Implementa responsividade em Home & Adiciona foto

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,22 +1,39 @@
 import './Home.css';
-import '../TechCarousel'
 import TechCarousel from '../TechCarousel';
+import fotoPerfil from "../../assets/mateus-profile.webp";
 
-export default function Home () {
+export default function Home() {
   return (
     <div className="home sobre-mim">
-      <span className='home-span'>// olá, eu sou o</span>
-      <h1 className='nome'>Mateus Oliveira,</h1>
-      <h2>estudante de Desenvolvimento de Software</h2>
-      <p className="resume">
-        Desenvolvedor em formação e participante do programa <span>Jovem Tech</span>. Tenho interesse em desenvolvimento de software, explorando tanto a criação de interfaces quanto a lógica por trás das aplicações. Atualmente, estudo <span>Java</span> e pratico desenvolvimento de interfaces com <span>React</span>, sempre buscando transformar ideias em soluções funcionais.
- 
-      </p>
-      <p className='embla-title'>Algumas das tecnologias que utilizo</p>
-      <TechCarousel />
-    </div>
-    
+      <div className="home-content">
+        <span className="home-span">// olá, eu sou o</span>
+        <h1 className="nome">Mateus Oliveira,</h1>
+        <h2>estudante de Desenvolvimento de Software</h2>
 
+        <p className="resume">
+          Desenvolvedor em formação e participante do programa <span>Jovem Tech</span>.
+          Tenho interesse em desenvolvimento de software, explorando tanto a criação de
+          interfaces quanto a lógica por trás das aplicações.
+        </p>
+        <p className="resume">
+          Atualmente, estudo desenvolvimento backend com <span>Java</span> e pratico
+          desenvolvimento de interfaces com <span>React</span>, sempre buscando
+          transformar ideias em soluções funcionais.
+        </p>
+
+        <p className="embla-title">Algumas das tecnologias que utilizo</p>
+      <TechCarousel />
+      </div>
       
-  )
+      <div className="foto-wrap">
+        <div className="foto">
+          <img
+            src={fotoPerfil}
+            alt="Foto de Mateus Oliveira"
+          />
+          <span className="foto-tag">mateus-profile.webp</span>
+        </div>
+      </div>
+    </div>
+  );
 }
